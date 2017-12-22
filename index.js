2943e35ec3952fed91ea2439a12f03c1dc18b923
yyf19871102 <854488704@qq.com>
2017-12-22 16:12:03 +0800

    add index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+	common      : require('./lib/common'),
+	Error       : require('./lib/Errors'),
+	MyValidator : require('./lib/MyValidator'),
+	logger      : require('./lib/logger'),
+	mongoosePlugin : require('./lib/mongoose_plugin'),
+	timeHelper  : require('./lib/timeHelper'),
+};
